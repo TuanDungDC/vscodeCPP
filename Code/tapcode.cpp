467407a4f85e960b58ff9b4373aec62c4fcde584
ADKnguDC <adkrngu216@gmail.com>
2022-10-04 17:08:36 +0700

    update

---
+++ Code/tapcode.cpp
@@ -0,0 +1,15 @@
+// In ra các số nguyên từ a đến b .
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b ;
+    cin >> a >> b ;
+    for(int i = a ; i <= b ; i++){
+        cout << i << " " ;
+    }
+    return 0 ;
+}
